44a57c27e387bd6803c3b8e53f0947c0d1024b39
Justin Zhao <jzhaojy@gmail.com>
2022-12-06 18:10:39 -0500

    minor update

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # day-scheduler
-FEATURE ROADMAP: 
+FEATURE ROADMAP: V1 Complete
     - Remove tasks from list (complete)
     - local storage of task list (complete)
     - "begin" button functionality: 
         - loads new page (complete)
         - loads list with timers & real time (complete)
         - display current task (complete)
-        - interactable (button or onclick) to 'complete' task
+        - interactable (button or onclick) to 'complete' task (complete)
+
+ADDITIONAL FUNCTIONALITY:
+    - Button to view history of completed tasks
+        - Reformat cart tasks to have date object
+        - Reformat time input to use date object
+    - MUI time selector when adding or editing tasks
 
 LOW PRIORITY:
-    - Use useCallback and react memo to optimize task list
+    - useCallback and react memo to optimize task list
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Header from "./components/Layout/Header";
 import Tasks from "./components/Tasks/Tasks";
 import Cart from "./components/Cart/Cart";
@@ -6,10 +6,11 @@ import CartProvider from "./store/CartProvider";
 import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import HeaderAlt from "./components/Layout/HeaderAlt";
 import TimerDisplay from "./components/Timer/TimerDisplay";
+import CartContext from "./store/cart-context";
 
 function TasksPage() {
   const [modalVisible, setModalVisible] = useState(false);
-
+  const cartContext = useContext(CartContext);
   function cartButtonHandler(event) {
     setModalVisible(true);
   }
@@ -17,6 +18,14 @@ function TasksPage() {
   function cartExitHandler(event) {
     setModalVisible(false);
   }
+
+  useEffect(()=> {
+    const storedDay = localStorage.getItem("cartDate");
+    const currentDay = JSON.stringify(new Date().getDay());
+    if (storedDay !== currentDay) {
+      cartContext.clearCart();
+    };
+  });
 
   return (
     <React.Fragment>
